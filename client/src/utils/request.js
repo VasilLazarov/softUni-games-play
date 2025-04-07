56d9ae8cc9160766f2419456a3c190393ee8900c
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-07 19:13:56 +0300

    Add abstract requester

---
+++ client/src/utils/request.js
@@ -0,0 +1,35 @@
+// with export if you dont want to use variant with .bind
+const request = async (method, url, data) => {
+    let options = {
+        method,
+    };
+    // if(method !== 'GET'){
+    //     options = {
+    //         method,
+    //     };
+    // }
+
+    if(data){
+        options = {
+            ...options,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        };
+    }
+    
+    const response = await fetch(url, options);
+
+    const result = await response.json();
+
+    return result;
+};
+
+export default {
+    get: request.bind(null, 'GET'),
+    // get: (...params) => request('GET', ...params),
+    post: request.bind(null, 'POST'),
+    put: request.bind(null, 'PUT'),
+    delete: request.bind(null, 'DELETE'),
+}
