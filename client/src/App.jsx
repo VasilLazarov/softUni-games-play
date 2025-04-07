bbae8433cd233c492f8455855b1788f036756576
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-07 23:17:04 +0300

    Add login handler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router";
+import { useState } from "react";
 
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
@@ -11,6 +12,12 @@ import GameEdit from "./components/game-edit/GameEdit";
 import "./App.css";
 
 function App() {
+    const [email, setEmail] = useState('');
+
+    const userLoginHandler = (email) => {
+        setEmail(email);
+    }
+
     return (
         <div id="box">
             <Header />
@@ -24,7 +31,7 @@ function App() {
                     <Route path="/games/create" element={<GameCreate />} />
                     <Route path="/games/:gameId/details" element={<GameDetails />} />
                     <Route path="/games/:gameId/edit" element={<GameEdit />} />
-                    <Route path="/login" element={<Login />} />
+                    <Route path="/login" element={<Login onLogin={userLoginHandler} />} />
                     <Route path="/register" element={<Register />} />
                 </Routes>
 
