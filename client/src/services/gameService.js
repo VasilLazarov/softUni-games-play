bd2218e73e6c6763579534a0fe9241258d2a8681
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-07 19:42:39 +0300

    List games in catalog page

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,10 +1,11 @@
-import { request } from "../utils/request";
+import request from "../utils/request";
+
 
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export default{
     async getAll(){
-        const result =  request.get(baseUrl);
+        const result = await request.get(baseUrl);
         // const result =  request('GET', baseUrl);
 
         const games = Object.values(result);
