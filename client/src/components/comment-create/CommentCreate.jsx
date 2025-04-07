b2056dc40ac8741d22413faa2d3e270c00744201
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-07 22:56:04 +0300

    Extracts comments related jsx in separate components

---
+++ client/src/components/comment-create/CommentCreate.jsx
@@ -0,0 +1,16 @@
+export default function CommentCreate() {
+    return (
+        // {/* <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
+        <article className="create-comment">
+            <label>Add new comment:</label>
+            <form className="form">
+                <textarea name="comment" placeholder="Comment......"></textarea>
+                <input
+                    className="btn submit"
+                    type="submit"
+                    value="Add Comment"
+                />
+            </form>
+        </article>
+    );
+}
