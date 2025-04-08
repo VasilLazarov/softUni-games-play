a15dbfbdf53cc3852483e2170f2d2b432b432647
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-08 18:51:42 +0300

    Redirect on successful logout

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,29 +1,30 @@
 // import { useEffect, useRef } from "react";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
-import request from "../utils/request"
+import request from "../utils/request";
 import { UserContext } from "../context/UserContext";
+import Logout from "../components/logout/Logout";
 
-const baseUrl = 'http://localhost:3030/users';
+const baseUrl = "http://localhost:3030/users";
 
 export const useLogin = () => {
     // const abortRef = useRef(new AbortController);
 
     const login = async (email, password) => {
         // const result = await request.post(
-        //     `${baseUrl}/login`, 
-        //     { email, password }, 
+        //     `${baseUrl}/login`,
+        //     { email, password },
         //     // { signal: abortRef.current.signal }
         // );
 
         // return result;
 
         return request.post(
-            `${baseUrl}/login`, 
-            { email, password }, 
+            `${baseUrl}/login`,
+            { email, password }
             // { signal: abortRef.current.signal }
         );
-    }
+    };
 
     // useEffect(() => {
     //     // const abortController = new AbortController();
@@ -36,29 +37,39 @@ export const useLogin = () => {
 
     return {
         login,
-    }
-}
+    };
+};
 
 export const useRegister = () => {
     const register = (email, password) => {
         return request.post(`${baseUrl}/register`, { email, password });
-    }
+    };
 
     return {
         register,
-    }
-}
+    };
+};
 
 export const useLogout = () => {
-    const { accessToken } = useContext(UserContext);
-    const options = {
-        headers: {
-            'X-Authorization': accessToken
+    const { accessToken, userLogoutHandler } = useContext(UserContext);
+
+    useEffect(() => {
+        if(!accessToken){
+            return;
         }
-    };
-    const logout = () => request.get(`${baseUrl}/logout`, null, options);
+
+        const options = {
+            headers: {
+                "X-Authorization": accessToken,
+            },
+        };
+        request.get(`${baseUrl}/logout`, null, options)
+            .then(() => {
+                userLogoutHandler();
+            });
+    }, [accessToken, userLogoutHandler]);
 
     return {
-        logout,
+        isLoggedOut: !!accessToken,
     }
-}
+};
