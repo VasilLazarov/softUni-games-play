c072bae685af4296069bee873a92602978741b48
Vasil Lazarov <105485479+VasilLazarov@users.noreply.github.com>
2025-04-09 11:47:48 +0300

    Check isOwner

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -6,12 +6,12 @@ import CommentsShow from "../comments-show/CommentsShow";
 import CommentCreate from "../comment-create/CommentCreate";
 import commentService from "../../services/commentService";
 import { useDeleteGame, useGame } from "../../api/gameApi";
-// import useAuth from "../../hooks/useAuth";
+import useAuth from "../../hooks/useAuth";
 
 export default function GameDetails() {
     const navigate = useNavigate();
     // const { email } = useContext(UserContext);
-    // const { email } = useAuth();
+    const { _id: userId } = useAuth();
     const [comments, setComments] = useState([]);
     const { gameId } = useParams();
     // const [game, setGame] = useState({});
@@ -31,29 +31,33 @@ export default function GameDetails() {
         //         setGame(result);
         //     });
 
-        commentService.getAll(gameId)
-            .then(result => {
-                setComments(result);
-            });
+        commentService.getAll(gameId).then((result) => {
+            setComments(result);
+        });
     }, [gameId]);
 
-
     const gameDeleteClickHandler = async () => {
-        const hasConfirm = confirm(`Are you sure you want to delete ${game.title} game?`);
+        const hasConfirm = confirm(
+            `Are you sure you want to delete ${game.title} game?`
+        );
 
-        if(!hasConfirm){
+        if (!hasConfirm) {
             return;
         }
 
         // await gameService.delete(gameId);
         await deleteGame(gameId);
 
-        navigate('/games');
-    }
+        navigate("/games");
+    };
 
     const commentCreateHandler = (newComment) => {
-        setComments(state => [...state, newComment]);
-    }
+        setComments((state) => [...state, newComment]);
+    };
+
+    const isOwner = userId === game._ownerId;
+    // console.log(userId);
+    // console.log(game.ownerId);
 
     return (
         <section id="game-details">
@@ -66,29 +70,29 @@ export default function GameDetails() {
                     <p className="type">{game.category}</p>
                 </div>
 
-                <p className="text">
-                    {game.summary}
-                </p>
+                <p className="text">{game.summary}</p>
 
-                <CommentsShow comments={comments}/>
+                <CommentsShow comments={comments} />
 
                 {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
-                <div className="buttons">
-                    <Link to={`/games/${gameId}/edit`} className="button">
-                        Edit
-                    </Link>
-                    <button 
-                        onClick={gameDeleteClickHandler} 
-                        className="button"
-                    >
-                        Delete
-                    </button>
-                </div>
+                {isOwner && (
+                    <div className="buttons">
+                        <Link to={`/games/${gameId}/edit`} className="button">
+                            Edit
+                        </Link>
+                        <button
+                            onClick={gameDeleteClickHandler}
+                            className="button"
+                        >
+                            Delete
+                        </button>
+                    </div>
+                )};
             </div>
 
-            <CommentCreate 
-                // email={email} 
-                gameId={gameId} 
+            <CommentCreate
+                // email={email}
+                gameId={gameId}
                 onCreate={commentCreateHandler}
             />
         </section>
